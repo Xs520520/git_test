73b64d9e497d4c2c98f4441f4a33f8817641f5aa
XuShuang <3196461197@qq.com>
2021-12-25 20:04:37 +0800

    更改hello 名字

--- src/com/xs/Demo1.java
+++ src/com/xs/Demo1.java
@@ -2,6 +2,6 @@ package com.xs;
 
 public class Demo1 {
     public static void main(String[] args) {
-        System.out.println("hello my name is lili");
+        System.out.println("hello my name is xushuang");
     }
 }
